b730d62094230ed5b0f5330a65728ba9091bb9d3
serhatprogramming <serhatprogramming@gmail.com>
2023-03-09 17:22:55 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,15 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import axios from "axios";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// const promise = axios
-//   .get("http://localhost:3001/notes")
-//   .then((response) => console.log(response.data));
-// console.log(promise);
-// const promise2 = axios.get("http://localhost:3001/foobar");
-// console.log(promise2);
 
 root.render(<App />);
