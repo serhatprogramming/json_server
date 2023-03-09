462f4db200e043c49e9a4a5108432407e8d70e35
serhatprogramming <serhatprogramming@gmail.com>
2023-03-09 10:48:10 -0500

    json_server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import axios from "axios";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// const promise = axios
+//   .get("http://localhost:3001/notes")
+//   .then((response) => console.log(response.data));
+// console.log(promise);
+// const promise2 = axios.get("http://localhost:3001/foobar");
+// console.log(promise2);
+
+root.render(<App />);
